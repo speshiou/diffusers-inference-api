603d4134e49cf44fe050a6392dd45e090f902310
pshaung <speshiou@gmail.com>
2024-02-26 10:01:32 +0800

    Migrate the main model to Juggernaut XL

--- predict.py
+++ predict.py
@@ -33,7 +33,7 @@ from utils import (
     resize_and_crop,
 )
 
-MODEL_ID = "Lykon/dreamshaper-xl-v2-turbo"
+MODEL_ID = "RunDiffusion/Juggernaut-XL-v9"
 DEPTH_MODEL_ID = "TencentARC/t2i-adapter-depth-midas-sdxl-1.0"
 FACE_ID_MODEL_CACHE = "./faceid-cache"
 FACE_ID_MODEL_URL = "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid_sdxl.bin?download=true"
